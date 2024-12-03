240364f50cd8e80c44a61566356d8469e8178da2
BenchPress200 <jms0538@naver.com>
2024-12-03 18:13:32 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 }
 
